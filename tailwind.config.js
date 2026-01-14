6ae661680eb44549bafe0a1c80f6ba8b48c37615
Gibran Raja Aulia Putranami <gibrannami43@gmail.com>
2026-01-14 21:05:48 +0700

    Initial commit - portfolio website

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6D28D9',
+        secondary: '#2563EB',
+        accent: '#F97316',
+      },
+    },
+  },
+  plugins: [],
+}
